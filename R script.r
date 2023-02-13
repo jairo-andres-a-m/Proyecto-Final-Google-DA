f1a5c297f464d5681b90c2dfe165dba8ca14177f
Jairo off-Gia <jairoandresamaya@gmail.com>
2023-02-13 12:01:05 -0500

    path para la casa y para la office

--- R script.r
+++ R script.r
@@ -9,15 +9,19 @@ library(lubridate)
 # Importacion de datos
 #######################################
 
-activityday <- read_csv("C:/Users/JairoA/Documents/R_Studio_JAAM_Files/Capstone Project JAAM/Fitabase_Data_04122016-05122016/dailyActivity_merged.csv")
-sleepday <- read_csv("C:/Users/JairoA/Documents/R_Studio_JAAM_Files/Capstone Project JAAM/Fitabase_Data_04122016-05122016/sleepDay_merged.csv")
+path_pc_casa <- "C:/Users/JairoA/Documents/R_Studio_JAAM_Files/Capstone Project JAAM/Fitabase_Data_04122016-05122016"
+path_pc_office <- "D:/RStudio/Fitabase Data 4.12.16-5.12.16"
 
-calorieshour <- read_csv("C:/Users/JairoA/Documents/R_Studio_JAAM_Files/Capstone Project JAAM/Fitabase_Data_04122016-05122016/hourlyCalories_merged.csv")
-intensitieshour <- read_csv("C:/Users/JairoA/Documents/R_Studio_JAAM_Files/Capstone Project JAAM/Fitabase_Data_04122016-05122016/hourlyIntensities_merged.csv")
-stepshour <- read_csv("C:/Users/JairoA/Documents/R_Studio_JAAM_Files/Capstone Project JAAM/Fitabase_Data_04122016-05122016/hourlySteps_merged.csv")
+activityday <- read_csv(file.path(path_pc_office, "dailyActivity_merged.csv"))
+  
+sleepday <- read_csv(file.path(path_pc_office, "sleepDay_merged.csv"))
 
-weight <- read_csv("C:/Users/JairoA/Documents/R_Studio_JAAM_Files/Capstone Project JAAM/Fitabase_Data_04122016-05122016/weightLogInfo_merged.csv")
-heartratesecond <- read_csv("C:/Users/JairoA/Documents/R_Studio_JAAM_Files/Capstone Project JAAM/Fitabase_Data_04122016-05122016/heartrate_seconds_merged.csv")
+calorieshour <- read_csv(file.path(path_pc_office, "hourlyCalories_merged.csv"))
+intensitieshour <- read_csv(file.path(path_pc_office, "hourlyIntensities_merged.csv"))
+stepshour <- read_csv(file.path(path_pc_office, "hourlySteps_merged.csv"))
+
+weight <- read_csv(file.path(path_pc_office, "weightLogInfo_merged.csv"))
+heartratesecond <- read_csv(file.path(path_pc_office, "heartrate_seconds_merged.csv"))
 
 #######################################
 # 
@@ -30,3 +34,5 @@ sleepday <- sleepday %>%
 
 activityday <- activityday %>%
   inner_join(sleepday, by=c())
+
+?file.path()
